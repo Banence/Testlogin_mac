f32b408e308e55b8e702d16024a75f102723d415
Banence <philip.prestigio@gmail.com>
2023-07-15 20:14:07 +0300

    Added a little change to when a user is logged out of the site and to where to display the name of the user in the navbar

--- TestWebsite/index.php
+++ TestWebsite/index.php
@@ -43,17 +43,14 @@ if (isset($_SESSION["user_id"])) {
           <?php endif; ?>
         </li>
       </ul>
+      <?php if(isset($_SESSION["user_id"])) : ?>
+        <span class="navbar-text">
+          <?php echo $user["name"] ?>
+        </span>
+      <?php endif; ?>
     </div>
   </div>
 </nav>
-
-<div class="container">
-  <?php if(isset($_SESSION["user_id"])) : ?>
-    <h1>Hello, <?php echo $user["name"] ?></h1>
-  <?php else : ?>
-    <h1>You are not logged in</h1>
-  <?php endif; ?>
-</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
