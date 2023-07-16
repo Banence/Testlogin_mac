8428da2d33a87ff37c82a986604f57faae15383c
Banence <philip.prestigio@gmail.com>
2023-07-16 11:17:59 +0300

    Added a password visibility toggle button when logging in

--- TestWebsite/login.php
+++ TestWebsite/login.php
@@ -1,27 +1,26 @@
 <?php
-
 $is_invalid = false;
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    
+
     $mysqli = require __DIR__ . "/database.php";
-    
+
     $sql = sprintf("SELECT * FROM user
                     WHERE email = '%s'",
-                   $mysqli->real_escape_string($_POST["email"]));
-    
+        $mysqli->real_escape_string($_POST["email"]));
+
     $result = $mysqli->query($sql);
-    
+
     $user = $result->fetch_assoc();
-    
+
     if ($user) {
-        
+
         if (password_verify($_POST["password"], $user["password_hash"])) {
-            
+
             session_start();
-            
+
             session_regenerate_id();
-            
+
             $_SESSION["user_id"] = $user["id"];
 
             header("Location: index.php");
@@ -29,10 +28,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             exit;
         }
     }
-    
+
     $is_invalid = true;
 }
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -41,6 +39,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta charset="UTF-8">
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/73fca10ead.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <form method="post">
@@ -48,14 +47,10 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             <h1 id="h1">Login</h1>
             <input type="email" name="email" id="email"
                 value="<?= htmlspecialchars($_POST["email"] ?? "") ?>" placeholder="Email">
-            <div style="position: relative;">
+            <div class="pass_input">
                 <input type="password" name="password" id="password" placeholder="Password">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16" style="position: absolute; top: 50%; right: 10px; transform: translateY(-50%);">
-                    <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
-                    <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
-                </svg>
+                <i id="icon" class="fa-regular fa-eye-slash fa-lg" onclick="togglePasswordVisibility()"></i>
             </div>
-            
             <button id="signupbutton">Log in</button>
             <?php if ($is_invalid): ?>
                 <em class="error-message">Invalid login</em>
@@ -64,16 +59,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             <a href="signup.php">Don't have an account?</a>
         </div>
     </form>
-    
-    
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="show_password.js"></script>
 </body>
 </html>
-
-
-
-
-
-
-
-
---
+++ TestWebsite/show_password.js
@@ -0,0 +1,14 @@
+function togglePasswordVisibility() {
+    var passwordInput = document.getElementById("password");
+    var icon = document.getElementById("icon");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+    } else {
+        passwordInput.type = "password";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+    }
+}
